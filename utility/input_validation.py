51c0179868b2ae4d1bba68cdb08a6cf699a14e71
GrindelfP <grindelf.perlomutrovij@gmail.com>
2020-08-14 06:35:34 +0300

    fix bugs with decimals and second_number required as operands in lg and ln

--- utility/input_validation.py
+++ utility/input_validation.py
@@ -2,7 +2,7 @@ def input_validation(first_number, operator, second_number) -> list:
     errors = []
     if first_number is "":
         errors.append("First number is empty!")
-    if operator is not "!" and second_number is "":
+    if operator != "!" and operator != "lg" and operator != "ln" and second_number is "":
         errors.append("Second number is empty!")
 
     return errors
